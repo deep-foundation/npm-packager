a256d14d48c5b6c46cfe45eff4be045b93ce723d
Konstantin Dyachenko <drakonard@gmail.com>
2023-01-28 17:17:24 +0000

    Draft for next steps.

--- import-code.ts
+++ import-code.ts
@@ -3,6 +3,15 @@ async ({ deep, require, gql, data: { newLink } }) => {
   const packageName = pq?.value?.value;
   const requireResult = require('@deep-foundation/deeplinks/imports/packager');
   const packager = new requireResult.Packager(deep);
+  // const imported = packager.import(package);
+  // if (error) throw error;
+  // if (imported?.errors?.length) throw imported.errors;
+  // await deep.insert({
+  //   type_id: await deep.id('@deep-foundation/core', 'Contain'),
+  //   from_id: newLink.from_id,
+  //   to_id: imported.packageId,
+  // });
+  // return imported;
   return { 
     packageName,
     requireResult,
