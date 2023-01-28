0dfe6b4153346d20e5a15b716e1a4a1260b8be93
Konstantin Dyachenko <drakonard@gmail.com>
2023-01-28 17:10:33 +0000

    packageName is received, packager is created

--- import-code.ts
+++ import-code.ts
@@ -1,4 +1,15 @@
 async ({ deep, require, gql, data: { newLink } }) => { 
   const { data: [pq] } = await deep.select({ id: newLink.to_id });
-  return {};
+  const packageName = pq?.value?.value;
+  const requireResult = require('@deep-foundation/deeplinks/imports/packager');
+  const packager = new requireResult.Packager(deep);
+  return { 
+    packageName,
+    requireResult,
+    requireResultType: typeof requireResult,
+    // packager,
+    packagerType: typeof packager,
+    // packagerImport: packager?.import,
+    packagerImportType: typeof packager?.import
+  };
 }
