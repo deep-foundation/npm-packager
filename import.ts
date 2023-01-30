dbc4438ae2eb68da90e2fc68c47fc5322ed3289c
Konstantin Dyachenko <drakonard@gmail.com>
2023-01-30 13:45:12 +0000

    No more useless debug output.

--- import.ts
+++ import.ts
@@ -4,13 +4,13 @@ import { Packager } from '@deep-foundation/deeplinks/imports/packager';
 import * as fs from 'fs';
 import pkg from './deep.json';
 
-console.log(pkg.data[8]);
+// console.log(pkg.data[8]);
 
 const encoding = 'utf8';
 
 pkg.data[8].value.value = fs.readFileSync('install-code.ts', encoding);
 
-console.log(pkg.data[8]);
+// console.log(pkg.data[8]);
 
 fs.writeFileSync('deep.json', JSON.stringify(pkg, null, 2), encoding);
 
