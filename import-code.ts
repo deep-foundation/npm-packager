d057e26d99801e87488a766dba4d68c410d719d6
Konstantin Dyachenko <drakonard@gmail.com>
2023-01-30 11:56:35 +0000

    Refactoring.

--- import-code.ts
+++ import-code.ts
@@ -1,36 +1,48 @@
-async ({ deep, require, gql, data: { newLink } }) => { 
-  const { data: [pq] } = await deep.select({ id: newLink.to_id });
-  const packageName = pq?.value?.value;
-  
-  const os = require('os');
-  var fs = require('fs');
-  const { v4: uuid } = require('uuid')
-  
-  const baseTempDirectory = os.tmpdir();
-  const randomId = uuid();
-  const tempDirectory = [baseTempDirectory,randomId].join('/');
-  console.log(tempDirectory);
-  fs.mkdirSync(tempDirectory);
-  
-  const execSync = require('child_process').execSync;
-  const command = `npm i ${packageName}`;
-  const output = execSync(command, { 
-      encoding: 'utf-8',
-      cwd: tempDirectory
-  });
-  console.log(`${command}\n`, output);
-  
-  const packageFile = [tempDirectory, 'node_modules', packageName, 'deep.json'].join('/');
-  console.log(packageFile);
-  
-  const pkg = require(packageFile);
-  console.log(pkg);
-  
-  fs.rmSync(tempDirectory, { recursive: true, force: true });
+async ({ deep, require, gql, data: { newLink } }) => {
+  const fs = require('fs');
 
-  const requireResult = require('@deep-foundation/deeplinks/imports/packager');
-  const packager = new requireResult.Packager(deep);
-  const imported = await packager.import(pkg);
-  if (imported?.errors?.length) throw imported.errors;
+  const makeTempDirectory = () => {
+    const os = require('os');
+    const { v4: uuid } = require('uuid');
+    
+    const baseTempDirectory = os.tmpdir();
+    const randomId = uuid();
+    const tempDirectory = [baseTempDirectory,randomId].join('/');
+    fs.mkdirSync(tempDirectory);
+    console.log(tempDirectory);
+    return tempDirectory;
+  };
+  const npmInstall = (packageName, tempDirectory) => {
+    const execSync = require('child_process').execSync;
+
+    const command = `npm i ${packageName}`;
+    const output = execSync(command, { 
+        encoding: 'utf-8',
+        cwd: tempDirectory
+    });
+    console.log(`${command}\n`, output);
+    return output;
+  };
+  const getPackage = (tempDirectory, packageName) => {
+    const packageFile = [tempDirectory, 'node_modules', packageName, 'deep.json'].join('/');
+    console.log(packageFile);
+    const pkg = require(packageFile);
+    console.log(pkg);
+    return pkg;
+  };
+  const deepImport = async (pkg) => {
+    const packager = new (require('@deep-foundation/deeplinks/imports/packager')).Packager(deep);
+    const imported = await packager.import(pkg);
+    if (imported?.errors?.length) throw imported.errors;
+    return imported;
+  };
+
+  const { data: [packageQuery] } = await deep.select({ id: newLink.to_id });
+  const packageName = packageQuery?.value?.value;
+  const tempDirectory = makeTempDirectory();
+  npmInstall(packageName, tempDirectory);
+  const pkg = getPackage(tempDirectory, packageName);
+  fs.rmSync(tempDirectory, { recursive: true, force: true });
+  const imported = await deepImport(pkg);
   return imported;
 }
