f0226ae4357397064568a3cf87769a026cb9d025
Konstantin Dyachenko <drakonard@gmail.com>
2023-02-02 16:49:34 +0000

    Added a draft from previous code version.

--- publish-code.ts
+++ publish-code.ts
@@ -30,7 +30,7 @@ async ({ deep, require, gql, data: { newLink } }) => {
     console.log(pkg);
     return pkg;
   };
-  
+
   const { data: [packageQuery] } = await deep.select({ id: newLink.to_id });
   const packageName = packageQuery?.value?.value;
   if (!packageName) {
@@ -39,6 +39,29 @@ async ({ deep, require, gql, data: { newLink } }) => {
   const tempDirectory = makeTempDirectory();
   npmInstall(packageName, tempDirectory);
   const pkg = getPackage(tempDirectory, packageName);
+
+  // const npmPckgPath = [pckgDir,'package.json'].join('/');
+  // const npmPckgJSON = fs.readFileSync(npmPckgPath, { encoding: 'utf-8' });
+  // let npmPckg;
+  // let nextVersion;
+  // if (!npmPckgJSON) {
+  //   errors.push('package.json not founded in installed package');
+  //   return { errors };
+  // } else {
+  //   npmPckg = JSON.parse(npmPckgJSON);
+  //   npmPckg.version = nextVersion = semver.inc(npmPckg?.version || '0.0.0', 'patch');
+  // }
+  // await deep.update({
+  //   link: {
+  //     type_id: { _eq: _ids?.['@deep-foundation/core']?.PackageVersion },
+  //     to_id: { _eq: id },
+  //   },
+  // }, { value: nextVersion }, { table: 'strings' });
+  // fs.writeFileSync(npmPckgPath, JSON.stringify(npmPckg), { encoding: 'utf-8' });
+  // const deepPckgContent = await packager.export({ packageLinkId: id });
+  // fs.writeFileSync([pckgDir,'deep.json'].join('/'), JSON.stringify(deepPckgContent), { encoding: 'utf-8' });
+  // child_process.execSync(`cd ${pckgDir}; npm publish;`,{stdio:[0,1,2]});
+
   fs.rmSync(tempDirectory, { recursive: true, force: true });
   // const exported = await deepExport();
   // return exported;
