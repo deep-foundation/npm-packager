671991dd00956d799433f4277a522975fe0a254a
Konstantin Dyachenko <drakonard@gmail.com>
2023-03-13 22:47:22 +0000

    Test updated to use real data.

--- test.ts
+++ test.ts
@@ -6,15 +6,22 @@ describe('packager tests', () => {
     const searchPackages = async (query) => {
       const deepPackageKeyword = 'deep-package';
       const textParameter = encodeURIComponent(`${query} keywords:${deepPackageKeyword}`);
-      const response = await fetch(`https://registry.npmjs.com/-/v1/search?text=${textParameter}`);
+      const url = `https://registry.npmjs.com/-/v1/search?text=${textParameter}`;
+      const response = await fetch(url);
       const data = await response.json();
       return data;
     };
 
-    const query = 'test';
-    const data = await searchPackages(query) as any;
-    console.log(data);
-    expect(data.objects.length).toBe(0);
-    expect(data.total).toBe(0);
+    const query1 = 'test';
+    const data1 = await searchPackages(query1) as any;
+    console.log(JSON.stringify(data1, null, 2));
+    expect(data1.objects.length).toBe(0);
+    expect(data1.total).toBe(0);
+
+    const query2 = '@deep-foundation/pow';
+    const data2 = await searchPackages(query2) as any;
+    console.log(JSON.stringify(data2, null, 2));
+    expect(data2.objects.length).toBe(1);
+    expect(data2.total).toBe(1);
   });
 });
