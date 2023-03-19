e9e9f48311c2c7ce0589301f4f15a46d20e84345
Konstantin Dyachenko <drakonard@gmail.com>
2023-03-19 00:55:05 +0000

    Handlers code is moved.

--- build.ts
+++ build.ts
@@ -4,10 +4,10 @@ import pkg from './deep.json';
 const encoding = 'utf8';
 
 const [installCode] = pkg.data.filter(l => l.id === "installCode");
-installCode.value.value = fs.readFileSync('install-code.ts', encoding);
+installCode.value.value = fs.readFileSync('handlers/install-code.ts', encoding);
 
 const [publishCode] = pkg.data.filter(l => l.id === "publishCode");
-publishCode.value.value = fs.readFileSync('publish-code.ts', encoding);
+publishCode.value.value = fs.readFileSync('handlers/publish-code.ts', encoding);
 
 fs.writeFileSync('deep.json', JSON.stringify(pkg, null, 2), encoding);
 
